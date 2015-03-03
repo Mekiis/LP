b2348c2ac6829e30c0052915c7aa9da1197547f7
SJ <j.simon01.c@gmail.com>
2015-03-03 11:42:45 +0100

    Add Unity and Windows phone projects

---
+++ UNITY/EXO/Demo/Assets/BougeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BougeScript : MonoBehaviour {
+
+	public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+	}
+}
---
+++ UNITY/EXO/SolarSystem/Assets/Script/MovePlanet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlanet : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public float speedParent = 1.0f;
+	public Transform parent = null;
+
+	private float desiredDistance = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		if(parent)
+			desiredDistance = Vector3.Distance (parent.position, transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+		if (parent) {
+			transform.RotateAround(parent.position, parent.up, speedParent * Time.deltaTime);
+			
+			float currentDistance = Vector3.Distance (parent.position, transform.position);
+			Vector3 towardTarget = transform.position - parent.position;
+			transform.position += (desiredDistance - currentDistance) * towardTarget.normalized;
+		}
+	}
+}
